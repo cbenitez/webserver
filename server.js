a20f3bf825cdd4b325e16f19106a33635132346a
cbenitez10 <cbenitezdiaz@gmail.com>
2018-06-27 22:45:48 -0400

    actualizacion

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use( express.static( __dirname + '/public') );
+
+app.set('view engine', 'hbs');
+
+app.get( '/', ( req, res ) => {
+    res.render('home',{
+        nombre: 'Christian',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000');
+});
