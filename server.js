41ad2cb67bb4623d6020a38c35007b5a8414d567
cbenitez10 <cbenitezdiaz@gmail.com>
2018-07-02 22:22:50 -0400

    actualizacion

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
 
 app.use( express.static( __dirname + '/public') );
 
+//partials
+hbs.registerPartials( __dirname + '/views/partials' );
 app.set('view engine', 'hbs');
 
 app.get( '/', ( req, res ) => {
-    res.render('home',{
-        nombre: 'Christian',
-        anio: new Date().getFullYear()
-    });
+	res.render('home',{
+		title: 'Web server | Demo',
+		nombre: 'christian'
+	});
+});
+
+app.get( '/about', (req, res) => {
+	res.render('about', {
+		title: 'About | Demo',
+		request: req.url
+	});
 });
 
 app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+	console.log('Escuchando peticiones en el puerto 3000');
 });
