b7f7b3516fab69eb7b8ce4657a92efe07c12fdd6
Christian Benitez <cbenitez@sd.com.py>
2018-07-03 12:14:33 -0400

    puerto

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 
 require('./hbs/helpers');
 
+const port = process.env.PORT || 3000;
+
 app.use( express.static( __dirname + '/public') );
 
 //partials
@@ -24,6 +26,6 @@ app.get( '/about', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Escuchando peticiones en el puerto 3000');
+app.listen( port, () => {
+	console.log('Escuchando peticiones en el puerto ' + port);
 });
